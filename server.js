0026c6618d8552a10564aac11e2259f9fa96452a
Carlos Gonzalez <carlos.gonzalez923@gmail.com>
2024-11-16 16:40:55 -0500

    correcion cors 2.0

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/database.js";
+
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import inventoryRoutes from "./routes/inventoryRoutes.js";
 
 dotenv.config();
-console.log("JWT_SECRET:", process.env.JWT_SECRET);
 
 const app = express();
 
@@ -16,26 +16,12 @@ const app = express();
 connectDB();
 
 // Middleware
-const allowedOrigins = [
-  process.env.CORS_LOCAL,
-  process.env.CORS_DEPLOY, // Replace with your actual frontend domain when deployed
-];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-  optionsSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: process.env.CORS_LOCAL,
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Routes
@@ -44,5 +30,5 @@ app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/inventory", inventoryRoutes);
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
